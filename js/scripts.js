4f6109ec21ae90c09eb7a4b929661ab4d3922d54
vijayvuyyuru <vijaykvuyyuru@gmail.com>
2019-12-02 18:12:32 -0600

    added contact me form and automatic project updating from github

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,39 @@ function navMenu() {
       x.style.display = "block";
     }
   }
+
+  function getJSON(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      const status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
+getJSON('https://api.github.com/users/vijayvuyyuru/repos', (err, data) => {
+    if (err) {
+       return;
+    }
+    const ul = document.createElement('ul');
+    ul.setAttribute('class','projectList');
+    for (let i = 0; i < data.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = "<a href='" + data[i].html_url + "' target='_blank'>" + data[i].name + "</a>";
+        ul.appendChild(li);
+        const desc = document.createElement('ul');
+        desc.setAttribute('class','projectList');
+        const descLi = document.createElement('li');
+        descLi.innerHTML = data[i].description;
+        desc.appendChild(descLi);
+        ul.appendChild(desc);
+    }
+    document.getElementById('projects-list').appendChild(ul);
+});  
